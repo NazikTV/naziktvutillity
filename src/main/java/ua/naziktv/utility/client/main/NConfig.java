f253a5fbf53266785cc82a2fb29929e2cbb8f48f
NazikTV <naziktv.da@gmail.com>
2015-10-09 23:15:57 +0300

    Test

---
+++ src/main/java/ua/naziktv/utility/client/main/NConfig.java
@@ -0,0 +1,52 @@
+package ua.naziktv.utility.client.main;
+
+
+import DummyCore.Utils.IDummyConfig;
+import net.minecraftforge.common.config.Configuration;
+import ua.naziktv.utility.API.NLoger;
+
+/**
+ * Created by NazikTV on 21.08.2015.
+ */
+public class NConfig implements IDummyConfig {
+    //public static Configuration cfg;
+    public static boolean ECGE, //enable craft GoldEgg
+                          ECV,  //enable craft Villeger
+                          ECN,  //enable craft Nazik entity
+                          ECED, //enable craft Ench Diamont
+                          ECEDA, //enable craft Ench Diamont Armor
+                          ECSC, //enable craft string from coton
+                          ECTB, //enable craft TestBlock
+                          ECEE, //enable craft EnchEmerald
+                          EC,   //enable all craft
+                          EM,   //enable all mod
+                          EDEBUG, //enable debug
+                          ESF;  //enable spawn fire when firearms
+    public static Configuration config1;
+    @Override
+    public void load(Configuration config) {
+        NLoger.DEBUG("Load config");
+        config.load();
+        //public static
+        ECGE = config.get("Enablecraft", "ECGE", true).getBoolean();
+        ECV = config.get("Enablecraft", "ECVillager", true).getBoolean();
+        ECN = config.get("Enablecraft", "ECNazikEntity", true).getBoolean();
+        ECED = config.get("Enablecraft", "ECED", true).getBoolean();
+        ECEDA = config.get("Enablecraft", "ECEDArmor", true).getBoolean();
+        ECSC = config.get("Enablecraft", "ECStringFromCoton", true).getBoolean();
+        ECTB = config.get("Enablecraft", "ECTestblock", true).getBoolean();
+        ECEE = config.get("Enablecraft", "ECEE", true).getBoolean();
+        EC = config.get("Enablecraft", "ECALL", true).getBoolean();
+        EM = config.get(Configuration.CATEGORY_GENERAL, "EALLMOD", true).getBoolean();
+        EDEBUG = config.get(Configuration.CATEGORY_GENERAL, "EnDEBUG", false).getBoolean();
+        ESF = config.get(Configuration.CATEGORY_GENERAL, "ESpawnFire", true).getBoolean();
+        //config.setCategoryComment("Enablecraft", "Enable craft from items");
+        config.save();
+        config1 = config;
+    }
+
+    public static void save(Configuration config){
+        config.save();
+    }
+
+}
